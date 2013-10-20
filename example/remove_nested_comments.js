17fd9166d302067621446104afab87d3c316c95c
Brian Sladek <brian.sladek@gmail.com>
2013-10-20 14:06:51 -0400

    third try

---
+++ example/remove_nested_comments.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+module.exports = {
+	fix_nested_comments: main
+};
+function main() {
+	var fs = require("fs");
+
+	fs.readFile("lib2.js", "utf8", function(error, data) {
+		var start_comment = false;
+		var end_comment = false;
+		var length = data.length;
+		for (var i = 0; i < length; i++) {
+			if (data.charAt(i) == "/" && data.charAt(i+1) == "*") {
+				if (start_comment === false) {
+					start_comment = true;
+					end_comment = false;
+				} else {
+					prev = data.substring(0, i);
+					rest = data.substring(i+2);
+					data = prev + rest;
+				}
+			}
+			if (data.charAt(i) == "*" && data.charAt(i+1) == "/") {
+				if (end_comment === false) {
+					start_comment = false;
+					end_comment = true;
+				} else {
+					prev = data.substring(0, i);
+					rest = data.substring(i+2);
+					data = prev + rest;
+				}
+			}
+		}
+		console.log(data);
+	});
+}
---
+++ example/simple.js
@@ -0,0 +1,5 @@
+function hi() {
+	return 'hello';
+}
+hi();
+console.log(fib(3));
